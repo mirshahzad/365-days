9957816f06fd37f96bf0374167d128c92f863d1a
mirshahzad <mirshahzad2012@gmail.com>
2023-10-03 21:18:30 +0500

    Writing a small story program

---
+++ story.py
@@ -0,0 +1,15 @@
+# In this file, the user will write the answer of some questions, and based on user input i.e. answers, 
+# this python code will generate an interesting story.
+
+# User Input or Questions for user to answer
+city = input('Write your city: ')
+name = input('Write your name: ')
+
+hobby = input('What do you like, e.g. painting, drawing, watching, etc.: ')
+food = input('What is your favorite food: ')
+
+# Output
+print('Once upon a time, in the peaceful town of', city, 'there lived a kind-hearted person named',name,"." 
+' He/She was known for his/her love of', hobby, 'and his/her favorite food was',food,"." 
+' One day, a group of curious friends approached', name,
+'seeking guidance on how to find happiness.', name, 'smiled and said To find true happiness, look within yourselves. The friends pondered these words, realizing that the answers they sought were already inside them.')
